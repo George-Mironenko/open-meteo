07ef8299c5c499cdbd5e619179cf74aed7346dab
georgijmironenko36@gmail.com <georgijmironenko36@gmail.com>
2025-07-23 12:34:57 +0300

    Написал комментарии для main.py

--- etl_code/main.py
+++ etl_code/main.py
@@ -8,6 +8,7 @@ from unit_converter import validate_date_format
 
 def main():
     try:
+        # Парсим аргументы из командной строки или из переменных окружения
         parser = argparse.ArgumentParser()
         parser.add_argument("--start_date", type=str, help="Start date (YYYY-MM-DD)")
         parser.add_argument("--end_date", type=str, help="End date (YYYY-MM-DD)")
@@ -17,9 +18,11 @@ def main():
         start_date = args.start_date or os.getenv("START_DATE")
         end_date = args.end_date or os.getenv("END_DATE")
 
+        # Если даты не заданы — ошибка
         if not start_date or not end_date:
             raise ValueError("Не указаны start_date и end_date")
 
+        # Проверка формата дат
         validate_date_format(start_date)
         validate_date_format(end_date)
 
@@ -30,6 +33,7 @@ def main():
         print("Ошибка ", error)
         exit(1)
     else:
+        # Запуск основного ETL-процесса
         extract_transform_load(start_date, end_date)
 
 if __name__ == "__main__":
